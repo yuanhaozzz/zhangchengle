38b3c709ed30a4e12c3bc35452e33a7962c9478b
yuanhao <yuanhao_zzz@163.com>
2026-05-22 11:16:18 +0800

    deploy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.resolve(__dirname, "dist")));
 
 app.get("*", (req, res) => {
   const html = fs.readFileSync(
-    path.resolve(__dirname, "dist/lowcode/index.html"),
+    path.resolve(__dirname, "dist/index.html"),
     "utf-8",
   );
   res.send(html);
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import FormView from '../views/FormView.vue'
-import ResultView from '../views/ResultView.vue'
-import HistoryView from '../views/HistoryView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import FormView from "../views/FormView.vue";
+import ResultView from "../views/ResultView.vue";
+import HistoryView from "../views/HistoryView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'form',
+      path: "/",
+      name: "form",
       component: FormView,
     },
     {
-      path: '/result',
-      name: 'result',
+      path: "/result",
+      name: "result",
       component: ResultView,
     },
     {
-      path: '/history',
-      name: 'history',
+      path: "/history",
+      name: "history",
       component: HistoryView,
     },
   ],
-})
+});
 
-export default router
+export default router;
