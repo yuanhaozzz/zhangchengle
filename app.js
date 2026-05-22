25f81e7b27b5e12c302fe5f9e8f416e95d1ecfb2
yuanhao <yuanhao_zzz@163.com>
2026-05-22 12:41:31 +0800

    deploy

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const express = require("express");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
-const path = require("path");
-const fs = require("fs");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.resolve(__dirname, "dist")));
+// 静态资源
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
-  const html = fs.readFileSync(
-    path.resolve(__dirname, "dist/zhang/index.html"),
-    "utf-8",
-  );
-  res.send(html);
+// ✅ 把 * 改成 /，解决报错！
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/zhang/index.html"));
 });
 
-app.listen(3010, () => {
-  console.log("start 3010");
+// 处理所有前端路由
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "dist/zhang/index.html"));
+});
+
+// 启动服务
+app.listen(3010, "0.0.0.0", () => {
+  console.log("✅ 服务启动成功：3010端口");
 });
